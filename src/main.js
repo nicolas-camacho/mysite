126ae47d8f36a0a97bd989eff7d4c25b8d55d12b
Nicolas Camacho <ncamacho1@cuc.edu.co>
2020-01-13 12:52:12 -0500

    :heavy_minus_sign: Deleting apollo and graphql things

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import apolloProvider from './apolloProvider'
 
 
 import './assets/styles/main.css'
@@ -10,6 +9,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  provide: apolloProvider.provide(),
   render: h => h(App)
 }).$mount('#app')
